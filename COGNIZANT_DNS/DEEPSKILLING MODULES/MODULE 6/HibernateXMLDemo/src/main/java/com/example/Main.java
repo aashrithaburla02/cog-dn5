a1dfed6bd2b2daad2393c88f923e2e7575bd1290
Aashritha <aashrithaburla02@gmail.com>
2026-07-08 11:49:08 +0530

    Add files via upload

---
+++ COGNIZANT_DNS/DEEPSKILLING MODULES/MODULE 6/HibernateXMLDemo/src/main/java/com/example/Main.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        Employee emp = new Employee();
+
+        emp.setId(101);
+        emp.setName("Aash");
+        emp.setSalary(50000);
+
+        session.save(emp);
+
+        tx.commit();
+
+        session.close();
+
+        factory.close();
+
+        System.out.println("Employee inserted successfully");
+    }
+
+}
