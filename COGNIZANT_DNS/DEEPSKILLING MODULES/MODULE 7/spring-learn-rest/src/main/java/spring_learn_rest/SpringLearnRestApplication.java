d75642fc86b532f32fc3e12a6703301e107152fc
Aashritha <aashrithaburla02@gmail.com>
2026-07-15 14:21:17 +0530

    Add files via upload

---
+++ COGNIZANT_DNS/DEEPSKILLING MODULES/MODULE 7/spring-learn-rest/src/main/java/spring_learn_rest/SpringLearnRestApplication.java
@@ -0,0 +1,13 @@
+package spring_learn_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnRestApplication.class, args);
+    }
+
+}
