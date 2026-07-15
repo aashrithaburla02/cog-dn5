9778741826bd4687813f0b3c310eae5bedf731aa
Aashritha <aashrithaburla02@gmail.com>
2026-07-15 15:05:15 +0530

    Add files via upload

---
+++ COGNIZANT_DNS/DEEPSKILLING MODULES/MODULE 7/spring-country-rest/src/main/java/com/cognizant/spring_country_rest/SpringCountryRestApplication.java
@@ -0,0 +1,12 @@
+package com.cognizant.spring_country_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCountryRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCountryRestApplication.class, args);
+    }
+}
